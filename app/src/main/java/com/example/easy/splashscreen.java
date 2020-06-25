7e2fb6715533f0039179f5741259a95efe4423ce
supermanabby <1995.abhaysingh@gmail.com>
2020-06-25 20:51:26 +0530

    easy app modified on 25 June

---
+++ app/src/main/java/com/example/easy/splashscreen.java
@@ -0,0 +1,18 @@
+package com.example.easy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        startActivity(new Intent(this, MainActivity.class));
+
+    }
+}
